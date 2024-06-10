0ad910f90b5ae8ed785009df7d983e8aed8573d2
Sang Nguyen <nguyenvansang6405@gmail.com>
2024-06-10 12:04:16 +0700

    Tự động cập nhật mức độ hoàn thành đơn hàng
    Khi hết hạn trả hàng thì không hiện nút Trả hàng, nút Đã nhận hàng

--- src/main/java/com/nhom14/webbookstore/controller/admin/AdminOrderController.java
+++ src/main/java/com/nhom14/webbookstore/controller/admin/AdminOrderController.java
@@ -1,6 +1,8 @@
 package com.nhom14.webbookstore.controller.admin;
 
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 import com.nhom14.webbookstore.entity.*;
@@ -26,96 +28,195 @@ public class AdminOrderController {
 	private ProfitService profitService;
 	private InfoReturnOrderService infoReturnOrderService;
 	private NotificationService notificationService;
-	
+
 	@Autowired
 	public AdminOrderController(OrderService orderService, BookService bookService,
-                                PaymentStatusService paymentStatusService, OrderItemService orderItemService, BookImportService bookImportService, RevenueService revenueService, ProfitService profitService, InfoReturnOrderService infoReturnOrderService, NotificationService notificationService) {
+								PaymentStatusService paymentStatusService, OrderItemService orderItemService, BookImportService bookImportService, RevenueService revenueService, ProfitService profitService, InfoReturnOrderService infoReturnOrderService, NotificationService notificationService) {
 		super();
 		this.orderService = orderService;
 		this.bookService = bookService;
 		this.paymentStatusService = paymentStatusService;
-        this.orderItemService = orderItemService;
-        this.bookImportService = bookImportService;
-        this.revenueService = revenueService;
-        this.profitService = profitService;
-        this.infoReturnOrderService = infoReturnOrderService;
-        this.notificationService = notificationService;
-    }
-	
+		this.orderItemService = orderItemService;
+		this.bookImportService = bookImportService;
+		this.revenueService = revenueService;
+		this.profitService = profitService;
+		this.infoReturnOrderService = infoReturnOrderService;
+		this.notificationService = notificationService;
+	}
+
 	@GetMapping("/manageorders")
 	public String manageOrders(@RequestParam(value = "status", required = false) Integer status,
-	@RequestParam(value = "search", required = false) String searchKeyword,
-	@RequestParam(value = "page", required = false, defaultValue = "1") Integer currentPage,
-	Model model,
-    HttpSession session) {
-		
+								@RequestParam(value = "search", required = false) String searchKeyword,
+								@RequestParam(value = "page", required = false, defaultValue = "1") Integer currentPage,
+								Model model,
+								HttpSession session,
+								RedirectAttributes redirectAttributes) {
+
 		Account admin = (Account) session.getAttribute("admin");
 
-	    // Kiểm tra xem admin đã đăng nhập hay chưa
-	    if (admin == null) {
-	        // Nếu chưa đăng nhập, chuyển hướng về trang đăng nhập
-	        return "redirect:/loginadmin";
-	    }
-	    
-	    List<Order> orders;
-	    int totalOrders;
-	    
-	    // Số danh mục hiển thị trên mỗi trang	
-        int recordsPerPage = 10;
-        int start;
-        int end;
-        int totalPages;
-        
-        if (status == null || (status == -1)) {
-        	orders = orderService.getAllOrders();
-        } else {
-        	orders = orderService.getOrdersByStatus(status);
-        }
-        
-        if (searchKeyword != null && !searchKeyword.isEmpty()) {
-        	orders = orderService.searchOrdersByKeyword(orders, searchKeyword);
-        }
-        
-        // Lấy tổng số lượng đơn hàng
-        totalOrders = orders.size();
-        
-        // Tính toán vị trí bắt đầu và kết thúc của đơn hàng trên trang hiện tại
-        start = (currentPage - 1) * recordsPerPage;
-        end = Math.min(start + recordsPerPage, totalOrders);
-        
-        // Lấy danh sách đơn hàng trên trang hiện tại
-        List<Order> ordersOnPage = orders.subList(start, end);
-        
-        // Tính toán số trang
-        totalPages = (int) Math.ceil((double) totalOrders / recordsPerPage);
-        
-        // Tổng số tất cả các đơn hàng
-        int totalAllOrders = orderService.getAllOrders().size();
-        
-        model.addAttribute("orders", ordersOnPage);
-        model.addAttribute("totalOrders", totalOrders);
-        model.addAttribute("totalPages", totalPages);
-        model.addAttribute("currentPage", currentPage);
-        model.addAttribute("totalAllOrders", totalAllOrders);
-        
-        return "admin/manageorders";
+		// Kiểm tra xem admin đã đăng nhập hay chưa
+		if (admin == null) {
+			// Nếu chưa đăng nhập, chuyển hướng về trang đăng nhập
+			return "redirect:/loginadmin";
+		}
+
+		List<Order> orders;
+		int totalOrders;
+
+		// Số danh mục hiển thị trên mỗi trang
+		int recordsPerPage = 10;
+		int start;
+		int end;
+		int totalPages;
+
+		if (status == null || (status == -1)) {
+			orders = orderService.getAllOrders();
+		} else {
+			orders = orderService.getOrdersByStatus(status);
+		}
+
+		if (searchKeyword != null && !searchKeyword.isEmpty()) {
+			orders = orderService.searchOrdersByKeyword(orders, searchKeyword);
+		}
+
+		// Lấy tổng số lượng đơn hàng
+		totalOrders = orders.size();
+
+		// Tính toán vị trí bắt đầu và kết thúc của đơn hàng trên trang hiện tại
+		start = (currentPage - 1) * recordsPerPage;
+		end = Math.min(start + recordsPerPage, totalOrders);
+
+		// Lấy danh sách đơn hàng trên trang hiện tại
+		List<Order> ordersOnPage = orders.subList(start, end);
+
+		// Tự động cập nhật mức độ hoàn thành đơn hàng
+		for (Order order : ordersOnPage) {
+			if (order.getStatus() == 4 &&
+					(order.getPaymentStatus().getStatus() == 3 || order.getPaymentStatus().getStatus() == 4)
+					&& order.getIsCompleted() != 2) {
+				// Nếu đơn hàng đã được hủy và đã được hoàn tiền, hoặc không cần thanh toán
+				order.setIsCompleted(2); //Đơn hàng đã hủy thành công
+			}
+
+			if (order.getStatus() == 7 &&
+					(order.getPaymentStatus().getStatus() == 3)
+					&& order.getIsCompleted() != 3) {
+				// Nếu đơn hàng đã trả hàng thành công (kèm theo đã được hoàn tiền)
+				order.setIsCompleted(3); //Đơn hàng đã trả thành công
+			}
+
+			// Tính số ngày giữa ngày giao hàng và ngày hiện tại
+			long daysBetween = -1; // Tức là chưa giao hàng
+			if (order.getDeliveryDate() != null){
+				LocalDateTime dateOrderLocalDateTime = order.getDeliveryDate().toLocalDateTime();
+				LocalDateTime today = LocalDateTime.now();
+				daysBetween = ChronoUnit.DAYS.between(dateOrderLocalDateTime.toLocalDate(), today.toLocalDate());
+
+				// Nếu đã giao hàng và quá 15 ngày
+				if (daysBetween != -1 && daysBetween > 15) {
+
+					if ((order.getStatus() == 3 || order.getStatus() == 10) &&
+							(order.getPaymentStatus().getStatus() == 1)
+							&& order.getIsCompleted() != 1) {
+						// Nếu đơn hàng là đã giao hoặc đã được nhận bởi khách hàng và đã được thanh toán
+						order.setIsCompleted(1); //Đơn hàng đã hoàn thành
+						// Kiểm tra xem Order đã tồn tại Revenue chưa
+						Revenue existingRevenue = revenueService.getRevenueByOrder(order);
+						if (existingRevenue == null) {
+							// Tính toán doanh thu
+							double revenueAmount = order.getTotalPrice();
+
+							// Tạo bản ghi doanh thu mới
+							Revenue revenue = new Revenue();
+							revenue.setOrder(order);
+							revenue.setRevenue(revenueAmount);
+							revenue.setDate(new Timestamp(System.currentTimeMillis()));
+							revenueService.addRevenue(revenue);
+						}
+
+						// Lấy tất cả các OrderItem của Order
+						List<OrderItem> orderItems = orderItemService.getOrderItemsByOrder(order);
+
+						for (OrderItem orderItem : orderItems) {
+							// Kiểm tra xem OrderItem đã tồn tại Profit chưa
+							Profit existingProfit = profitService.getProfitByOrderItem(orderItem);
+							if (existingProfit == null) {
+								// Lấy Book tương ứng với OrderItem
+								Book book = orderItem.getBook();
+
+								// Lấy đợt nhập sách đang bán của Book
+								BookImport bookImport = bookImportService.getBookImportByBookAndStatus(book, 1);
+
+								if (bookImport == null){
+									redirectAttributes.addAttribute("message", "Lỗi khi đang tự động cập nhật mức độ hoàn thành cho đơn hàng: Không tìm thấy đợt nhập sách!");
+									// Chuyển hướng về trang manageorderitems
+									return "redirect:/manageorderitems?orderId=" + order.getId();
+								}
+
+								// Kiểm tra xem số lượng còn lại có đủ để đáp ứng yêu cầu của OrderItem hay không
+								if (bookImport.getRemainingQuantity() >= orderItem.getQuantity()) {
+									// Trừ remaining_quantity và cập nhật BookImport
+									bookImport.setRemainingQuantity(bookImport.getRemainingQuantity() - orderItem.getQuantity());
+									bookImportService.updateBookImport(bookImport);
+								} else {
+									// Xử lý trường hợp không đủ sách...
+									redirectAttributes.addAttribute("message", "Lỗi khi đang tự động cập nhật mức độ hoàn thành cho đơn hàng, vui lòng thử lại sau!" + bookImport.getRemainingQuantity() +"----------" + orderItem.getQuantity());
+									// Chuyển hướng về trang manageorderitems
+									return "redirect:/manageorderitems?orderId=" + order.getId();
+								}
+
+								orderItem.setCostPrice(bookImport.getImportPrice());
+								orderItemService.updateOrderItem(orderItem);
+
+								// Tính toán lợi nhuận
+								double profitAmount = (orderItem.getSellPrice() - bookImport.getImportPrice()) * orderItem.getQuantity();
+
+								// Tạo bản ghi lợi nhuận mới
+								Profit profit = new Profit();
+								profit.setOrderItem(orderItem);
+								profit.setCostPrice(bookImport.getImportPrice());
+								profit.setSellPrice(orderItem.getSellPrice());
+								profit.setProfit(profitAmount);
+								profit.setDate(new Timestamp(System.currentTimeMillis()));
+								profitService.addProfit(profit);
+							}
+						}
+					}
+				}
+			}
+			orderService.updateOrder(order);
+		}
+
+		// Tính toán số trang
+		totalPages = (int) Math.ceil((double) totalOrders / recordsPerPage);
+
+		// Tổng số tất cả các đơn hàng
+		int totalAllOrders = orderService.getAllOrders().size();
+
+		model.addAttribute("orders", ordersOnPage);
+		model.addAttribute("totalOrders", totalOrders);
+		model.addAttribute("totalPages", totalPages);
+		model.addAttribute("currentPage", currentPage);
+		model.addAttribute("totalAllOrders", totalAllOrders);
+
+		return "admin/manageorders";
 	}
-	
+
 	@PostMapping("/updateorderstatus")
-	public String updateOrderStatus(@RequestParam("orderId") int orderId, 
+	public String updateOrderStatus(@RequestParam("orderId") int orderId,
 									@RequestParam("status") int status,
 									HttpSession session,
 									RedirectAttributes redirectAttributes) {
-	    Account admin = (Account) session.getAttribute("admin");
+		Account admin = (Account) session.getAttribute("admin");
 
-	    // Kiểm tra xem admin đã đăng nhập hay chưa
-	    if (admin == null) {
-	        // Nếu chưa đăng nhập, chuyển hướng về trang đăng nhập
-	        return "redirect:/loginadmin";
-	    }
+		// Kiểm tra xem admin đã đăng nhập hay chưa
+		if (admin == null) {
+			// Nếu chưa đăng nhập, chuyển hướng về trang đăng nhập
+			return "redirect:/loginadmin";
+		}
 
-	    // Lấy đơn hàng từ Service
-	    Order order = orderService.getOrderById(orderId);
+		// Lấy đơn hàng từ Service
+		Order order = orderService.getOrderById(orderId);
 
 		// Nếu là trạng thái đã giao thì thêm ngày giao hàng vô
 		// Kèm theo thanh toán sẽ tự động chuyển sang Đã thanh toán
@@ -168,11 +269,11 @@ public class AdminOrderController {
 			infoReturnOrderService.updateInfoReturnOrder(infoReturnOrder);
 		}
 
-	    // Cập nhật trạng thái đơn hàng
-	    order.setStatus(status);
+		// Cập nhật trạng thái đơn hàng
+		order.setStatus(status);
 
-	    // Cập nhật đơn hàng thông qua Service
-	    orderService.updateOrder(order);
+		// Cập nhật đơn hàng thông qua Service
+		orderService.updateOrder(order);
 
 		Notification notification = new Notification();
 		// Chỗ này là chuỗi ghi nội dung ví dụ:
@@ -186,8 +287,8 @@ public class AdminOrderController {
 		notification.setTriggerUser(admin);
 		notification.setSentTime(new Timestamp(System.currentTimeMillis()));
 		notification = notificationService.save(notification);
-	    // Chuyển hướng về trang manageorderitems
-	    return "redirect:/manageorderitems?orderId=" + orderId;
+		// Chuyển hướng về trang manageorderitems
+		return "redirect:/manageorderitems?orderId=" + orderId;
 	}
 
 	public String createNotificationContent(int orderId, int status, PaymentStatus paymentStatus) {
@@ -253,8 +354,8 @@ public class AdminOrderController {
 
 	@PostMapping("/updatepaymentstatus")
 	public String updatePaymentStatus(@RequestParam("orderId") int orderId,
-									@RequestParam("paymentstatus") int paymentstatus,
-									HttpSession session) {
+									  @RequestParam("paymentstatus") int paymentstatus,
+									  HttpSession session) {
 		Account admin = (Account) session.getAttribute("admin");
 
 		// Kiểm tra xem admin đã đăng nhập hay chưa
